0fedc482d705eda07afefd7e66f960873c60c3e9
Ken Murchison <murch@fastmail.com>
2021-10-07 08:18:57 -0400

    Sieve iMIP: no longer using iSchedule over UNIX socket

--- Cassandane/Cyrus/TestCase.pm
+++ Cassandane/Cyrus/TestCase.pm
@@ -96,7 +96,6 @@ sub new
         adminstore => 0,
         gen => 1,
         deliver => 0,
-        imip => 0,
         jmap => 0,
         install_certificates => 0,
     };
@@ -505,8 +504,6 @@ sub _create_instances
         $self->{instance}->add_services(@{$want->{services}});
         $self->{instance}->_setup_for_deliver()
             if ($want->{deliver});
-        $self->{instance}->_setup_for_imip()
-            if ($want->{imip});
 
         if ($want->{replica} || $want->{csyncreplica})
         {
